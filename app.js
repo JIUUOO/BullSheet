28b040ed150c226d2babed6a9adcc62f92f070c2
JIUUOO <nninjiuuoo@gmail.com>
2024-06-26 10:01:51 +0900

    add home and sheet pages in terms of express, perform numeric calcuation

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post("/sheet", (req, res) => {
+  const budget = req.body.budget;
+  res.render("sheet", { budget });
 });
 
 app.listen(3000, () => {
